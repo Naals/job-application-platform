cf1cc2d42490dae530809fbf2e4e2a47a0c48dc6
Aslan <tynyshtykaslan06@gmail.com>
2026-04-10 22:13:09 +0500

    feat(gateway): circuit breaker + fallback

---
+++ api-gateway/src/main/java/com/project/apigateway/controller/FallbackController.java
@@ -0,0 +1,23 @@
+package com.project.apigateway.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/fallback")
+    public Mono<ResponseEntity<Map<String, String>>> fallback() {
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(Map.of(
+                        "status",  "503",
+                        "message", "Service temporarily unavailable. Please try again later."
+                )));
+    }
+}
