93f3f75b068be0564404584013f2266cbb3c61e9
Aslan <tynyshtykaslan06@gmail.com>
2026-04-12 23:26:23 +0500

    feat(jobs): scaffold + pom.xml

--- job-posting-service/src/main/java/com/project/jobpostingservice/JobPostingServiceApplication.java
+++ job-posting-service/src/main/java/com/project/jobpostingservice/JobPostingServiceApplication.java
@@ -2,8 +2,10 @@ package com.project.jobpostingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class JobPostingServiceApplication {
 
     public static void main(String[] args) {
