45180ffa518cf0ffbd1779d527074266b45d368e
Aslan <tynyshtykaslan06@gmail.com>
2026-04-15 23:08:20 +0500

    feat(resume): scaffold + pom.xml

--- resume-service/src/main/java/com/project/resumeservice/ResumeServiceApplication.java
+++ resume-service/src/main/java/com/project/resumeservice/ResumeServiceApplication.java
@@ -2,8 +2,10 @@ package com.project.resumeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ResumeServiceApplication {
 
     public static void main(String[] args) {
